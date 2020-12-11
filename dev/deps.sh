8f2fe9002c0619545a5881a5890eca1f77014371
Charles Goncalves <charles.fg@gmail.com>
2020-12-11 15:38:36 +0000

    changing the vim setup and adding developing tools

---
+++ dev/deps.sh
@@ -0,0 +1,3 @@
+sudo apt-get install python-pip 
+sudo pip install compiledb
+sudo apt-get install -y build-essential   manpages-posix-dev
--- vim/setup.sh
+++ vim/setup.sh
@@ -1,3 +1,9 @@
+function link_to_home(){
+    local f=$1
+    shift
+    ln -s $(readlink -f ${f}) ~/${f}
+}
+
 vim --version
 sudo apt install software-properties-common build-essential python3-dev gcc python-dev cmake3
 # in newer then ubuntu 14.04
@@ -13,15 +19,18 @@ echo "Install Plugger"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo "Copying .vimrc"
-cp -v .vimrc ~/
+echo "Linking .vimrc"
+link_to_home .vimrc
 
 
 echo "Open vim and install the plugins with 'PlugInstall'"
-read -p "Are you sure? " -n 1 -r
+read -p "Opening VIM, press any key " -n 1 -r
 vim
 
 echo "Install the YCM with support to clang bidings" 
 cd ~/.vim/plugged/YouCompleteMe/
 ./install.py --clangd-completer
+
+echo "Setup the default .ycm_extra_conf.py "
+link_to_home .ycm_extra_conf.py
 
