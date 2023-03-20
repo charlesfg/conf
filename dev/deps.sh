04c3204208767e7e81a3cbfb6763de4a81e3a0d5
Charles Ferreira Goncalves <charles@cefetmg.br>
2023-03-20 18:55:01 -0300

    adding net-tools to the dev package

--- dev/deps.sh
+++ dev/deps.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y byobu
+sudo apt-get install -y byobu net-tools
 sudo apt-get install -y curl wget httpie  
 sudo apt-get install -y python-pip 
 sudo apt-get install -y build-essential   manpages-posix-dev
