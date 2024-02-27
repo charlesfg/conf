bc6e8cb1e13c1c3582212c8318e6c148d42f4780
Charles Goncalves <charles.fg@gmail.com>
2024-02-27 11:12:52 +0000

    adding -y to apt install

--- basic_setup.sh
+++ basic_setup.sh
@@ -1,4 +1,4 @@
-sudo apt-get install vim most curl byobu autojump 
+sudo apt-get install -y  vim most curl byobu autojump python-dev python3-dev
 sudo update-alternatives --set editor /usr/bin/vim.basic
 sudo update-alternatives --set pager /usr/bin/most
 
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,4 +1,4 @@
-sudo apt-get install git autojump zsh curl
+sudo apt-get install -y git autojump zsh curl
 % brew install  autojump curl
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
