760a933cde724a2c8eacf45bf31cf6b6d961e6b2
Charles Goncalves <charles.fg@gmail.com>
2024-03-05 15:45:25 +0000

    Changing the gcc.sh script to install available versions
    The new version search the available versions and instal them
    accordingly with its age (newer first) and attributing
    higher priorities to new versions.

--- dev/gcc.sh
+++ dev/gcc.sh
@@ -1,9 +1,77 @@
+#!/bin/bash
+
+# Function to ask for user confirmation
+confirm() {
+    while true; do
+        read -r -p "Do you wish to continue? [Y/n] " response
+        case "$response" in
+            [yY][eE][sS]|[yY]|"")
+                echo "Continuing..."
+                break
+                ;;
+            [nN][oO]|[nN])
+                echo "Exiting."
+                exit
+                ;;
+            *)
+                echo "Invalid input... please answer yes or no."
+        esac
+    done
+}
+
+
+# The rest of your script goes here
+echo "Your script continues..."
+
+
+
+echo "Searching for GCC version"
+gcc_v=()
+
+while IFS= read -r line; do
+    # Append each line to the array
+    gcc_v+=("$line")
+done < <(apt search gcc 2>/dev/null | grep 'gcc-[0-9]\+/' | cut -d/ -f1  | sort -Vr)
+
+echo "Avaliable versions of gcc and its priority:"
+echo 
+
+PRIORITY=100
+
+for g in "${gcc_v[@]}"; do
+    echo $g $PRIORITY
+    PRIORITY=$(($PRIORITY-10))
+done
+
+PRIORITY=100
+
+echo 
+
+# Call the confirm function
+confirm
+
+
+
+echo 
+echo "Installing repository and default system version"
+echo 
+
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 
-sudo apt-get install -y gcc g++ gcc-5 g++-5 gcc-6 g++-6 gcc-7 g++-7
+sudo apt-get install -y gcc g++ 
+
+confirm
+
 sudo update-alternatives --remove-all gcc
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 90 --slave /usr/bin/g++ g++ /usr/bin/g++-5
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 80 --slave /usr/bin/g++ g++ /usr/bin/g++-6
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 70 --slave /usr/bin/g++ g++ /usr/bin/g++-7
+
+for g in "${gcc_v[@]}"; do
+
+    echo "Installing $g version ... "
+    sudo apt-get install -y $g 
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/${g} $PRIORITY --slave /usr/bin/g++ g++ /usr/bin/${g}
+    PRIORITY=$(($PRIORITY-10))
+
+done
+
 sudo update-alternatives --config gcc
