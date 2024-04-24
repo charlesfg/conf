c48ad35f72555b075cbccb45a8c82ae97f054020
Charles Goncalves <charles.fg@gmail.com>
2024-04-24 23:01:10 +0100

    adding ansible scripts to setup server

---
+++ ansible/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+source "../shell_utilities/detect_os.sh"
+
+
+
+# Handle system-specific scripts (Option 1: Separate files)
+if [[ "$OS" == "Linux" ]]; then
+  # Run Linux specific script
+  sudo apt update
+  sudo apt install -y ansible
+
+elif [[ "$OS" == "Mac" ]]; then
+  # Run Mac specific script
+  brew install ansible
+fi
