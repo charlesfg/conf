7e29e1c78780686626d4154642eadbcbe56c234b
Charles Goncalves <charles.fg@gmail.com>
2024-05-01 12:42:09 +0100

    updating the script for ansible

--- ansible/invoke_playbooks.sh
+++ ansible/invoke_playbooks.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-ansible-playbook -i inventory.yaml --limit xen4 init_server.yml
-ansible-playbook -i inventory.yaml --limit xen4 oh_my_zsh.yml
+HOST=l1v4-18
+ansible-playbook -i inventory.yaml --limit l1v4-18 init_server.yml
+ansible-playbook -i inventory.yaml --limit l1v4-18 oh_my_zsh.yml
